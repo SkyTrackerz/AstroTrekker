9886e42ad7422541ea23157bbff52db4ea52e602
Robert Wetzler <42687232+ElRoberto13@users.noreply.github.com>
2024-02-19 15:09:05 -0800

    Correct callback typing

--- LimitSwitch/ILimitSwitch.py
+++ LimitSwitch/ILimitSwitch.py
@@ -1,9 +1,12 @@
 from abc import ABC, abstractmethod
+from typing import Callable
+
+
 class ILimitSwitch(ABC):
     
     @abstractmethod
     def isActive(self) -> bool:
         pass
 
-    def add_active_callback(self, stop_callback: Func):
+    def add_active_callback(self, stop_callback: Callable):
         pass
