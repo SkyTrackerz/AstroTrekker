7c8aeac66604b7dfeab5604b6f544432e8da03c9
Robert Wetzler <42687232+ElRoberto13@users.noreply.github.com>
2024-04-07 13:40:25 -0700

    Fix sky calculator loading

--- skyCalculator.py
+++ skyCalculator.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import pandas as pd
 from typing import Tuple, Union
 
-from skyfield.api import N, W, wgs84, load
+from skyfield.api import N, W, wgs84, Loader
 from skyfield.starlib import Star
 from skyfield.toposlib import GeographicPosition
 from skyfield.units import Angle
@@ -24,13 +24,15 @@ class SkyCalculator:
     """
     def __init__(self, location: Location):
          # TODO do this on statup
+        self.astro_data_dir = Environment.get_project_root() / STORAGE_DIR_PATH
+        self.skyfield_loader = Loader(self.astro_data_dir)
+        self.star_df = None
         self.load_astro_data()
         # Import planets
         self.earth = self.planets['earth']
         # Define current location
         self.loc: GeographicPosition = self.earth + wgs84.latlon(location.latitude, location.longitude)
         self.target = None
-        self.star_df = None
         logging.basicConfig(level=logging.WARNING)
     def set_target(self, target: Union[str, int]) -> bool:
         if target in self.planets:
@@ -41,10 +43,10 @@ class SkyCalculator:
             return True
         return False
 
-        
+
     def get_local_alt_az(self) -> Tuple[Angle, Angle]:
         # Get current time from skyfield
-        ts = load.timescale()
+        ts = self.skyfield_loader.timescale()
         t = ts.now()
 
         # Observe target object
@@ -54,22 +56,23 @@ class SkyCalculator:
         return alt, az
 
     def load_astro_data(self):
-        astro_data_dir = Environment.get_project_root() / STORAGE_DIR_PATH
-        self.planets = load('de421.bsp', astro_data_dir)
-        hippa_filepath = astro_data_dir / 'hipparcos_data.csv'
+        load = Loader('~/skyfield-data')
+        self.planets = self.skyfield_loader('de421.bsp')
+        hippa_filepath = self.astro_data_dir / 'hipparcos_data.csv'
 
         if os.path.exists(hippa_filepath):
             self.star_df = pd.read_csv(hippa_filepath)
             logging.info("Using cached star data")
-        elif asyncio.run(Environment.check_internet()):
-            with load.open(hipparcos.URL) as f:
-                self.star_df = hipparcos.load_dataframe(f)
-                self.star_df.to_csv(hippa_filepath)
-                logging.info("Loaded and saved star data from internet")
         else:
-            # Log a warning if the file is not found
-            logging.error(
-                f"The file {hippa_filepath} does not exist. Please ensure you have an internet connection to download the data.")
+            try:
+                with load.open(hipparcos.URL) as f:
+                    self.star_df = hipparcos.load_dataframe(f)
+                    self.star_df.to_csv(hippa_filepath)
+                    logging.info("Loaded and saved star data from internet")
+            except:
+                # Log a warning if the file is not found
+                logging.error(
+                    f"The file {hippa_filepath} does not exist. Please ensure you have an internet connection to download the data.")
 # Find the project root
 def find_project_root(relative_path=''):
     current = Path(__file__).parent  # Start from the current file location
