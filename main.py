597288158201dad4ad40ca1fb77742ed4d799ca2
Robert Wetzler <42687232+ElRoberto13@users.noreply.github.com>
2024-04-12 08:23:41 -0700

    Remote debugging

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 import logging.config
 
+
 import config
 from StarTracker.StarTrackerService import StarTrackerService
 from StarTracker.StarTracker import StarTracker
 from Motor.Motor import Motor
 from webapp.webapp import WebApp
+import argparse
+from remote_pdb import RemotePdb
 
 if __name__ == '__main__':
+
+    # Set up argument parsing
+    parser = argparse.ArgumentParser(description='Start the Flask application')
+    parser.add_argument('--debug', action='store_true', help='Enable remote debugger')
+    args = parser.parse_args()
+
     logging.config.fileConfig('logging.conf')
-    # This is how you get the logger from any class
     logger = logging.getLogger(__name__)
     logger.info("Starting!")
+
+
+    if args.debug:
+        logger.info("Remote debugger is enabled.")
+        RemotePdb('0.0.0.0', 4444).set_trace()
+
 
     star_tracker = StarTracker(
         turntable=Motor(config.TURNTABLE),
         turret=Motor(config.TURRET),
         spin=Motor(config.SPIN)
     )
+
     star_tracker_service = StarTrackerService(star_tracker)
     webapp = WebApp(star_tracker_service)
     webapp.run()
